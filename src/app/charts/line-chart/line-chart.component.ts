c460cca07a137f32509d3c68ee31d86e1a809a4f
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2020-10-20 22:50:43 +0300

    Add and conig pie chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+  
 
   ngOnInit(): void {
   }
--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350, 450, 180];
+  pieChartlabel: string[] = ['Logistics','Cost Of Goods Sold', 'Profit'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111111'
+    }
+  ];
+  pieChartType = 'doughnut';
+  
   ngOnInit(): void {
   }
 
